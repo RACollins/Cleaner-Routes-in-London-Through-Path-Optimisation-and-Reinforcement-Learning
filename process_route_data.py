2c1410ebfa3018cfa76f25316555d120f98a2d84
RACollins <35782077+RACollins@users.noreply.github.com>
2018-04-12 11:37:19 +0100

    Update process_route_data.py
    Added the ability to simplify the curve using the Ramer–Douglas–Peucker algorithm.

--- process_route_data.py
+++ process_route_data.py
@@ -1,8 +1,10 @@
 import gpxpy
 import pandas as pd
 import matplotlib.pyplot as plt
+from rdp import rdp
 
-gpx_file = open("/Users/richardcollins/Desktop/Warwick_uni/CS913/running-from-air-pollution-code/src/richard_strava/Lunch_Run.gpx", "r")
+
+gpx_file = open("/Users/richardcollins/Desktop/Warwick_uni/CS913/running-from-air-pollution-code/src/richard_strava/Run_to_work_Southbank.gpx", "r")
 
 gpx = gpxpy.parse(gpx_file)
 gpx_track = gpx.tracks[0]
@@ -21,11 +23,25 @@ track_coords = [[point.latitude,point.longitude, point.elevation]
                                     for segment in track.segments
                                         for point in segment.points]
 
-coords_df = pd.DataFrame(track_coords, columns = ['Latitude','Longitude','Altitude'])
+coords_df = pd.DataFrame(track_coords, columns = ['Latitude', 'Longitude', 'Altitude'])
+track_coords_reduced = rdp(coords_df[['Latitude', 'Longitude']], epsilon = 0.000001)
+coords_reduced_df = pd.DataFrame(track_coords_reduced, columns = ['Latitude', 'Longitude'])
+print("Full number of points: " + str(len(coords_df)))
+print("Reduced number of points: " + str(len(coords_reduced_df)))
 
-fig = plt.figure(figsize = (12, 9))
-coords_df.plot('Longitude', 'Latitude', color='#A00084', linewidth=1.5)
+
+#plot long and lat points
+plt.figure(figsize = (12, 9))
+#plt.subplot(121)
+plt.plot(coords_df['Longitude'], coords_df['Latitude'], color = "blue", linewidth = 2.5, alpha = 0.5)
+plt.plot(coords_reduced_df['Longitude'], coords_reduced_df['Latitude'], color = "red", linewidth = 2.5, alpha = 0.5)
 axes = plt.gca()
 axes.set_xlim([bounds.min_longitude-0.0005, bounds.max_longitude+0.0005])
 axes.set_ylim([bounds.min_latitude-0.0005, bounds.max_latitude+0.0005])
+
+'''plt.subplot(122)
+plt.plot(coords_reduced_df['Longitude'], coords_reduced_df['Latitude'], color='#A00084', linewidth=1.5)
+axes = plt.gca()
+axes.set_xlim([bounds.min_longitude-0.0005, bounds.max_longitude+0.0005])
+axes.set_ylim([bounds.min_latitude-0.0005, bounds.max_latitude+0.0005])'''
 plt.show()
