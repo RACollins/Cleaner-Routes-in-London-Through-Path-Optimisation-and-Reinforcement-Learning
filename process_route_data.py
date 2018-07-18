701110809ab983d49651f9f44966867bd90b40f0
RACollins <35782077+RACollins@users.noreply.github.com>
2018-07-18 15:40:34 +0100

    Update process_route_data.py
    Adding edge list function

--- process_route_data.py
+++ process_route_data.py
@@ -74,6 +74,19 @@ def clip_traj(traj, limits):
 def euclid_d(x1, y1, x2, y2):
     return(gpxpy.geo.haversine_distance(y1, x1, y2, x2))
 
+def find_start_node(traj, nodes):
+    closest_node = None
+    if len(traj) != 0:
+        start_lon = traj["Longitude"].values[0]
+        start_lat = traj["Latitude"].values[0]
+        smallest_d = math.inf
+        for index, row in nodes.iterrows():
+            d = euclid_d(start_lon, start_lat, row["lon"], row["lat"])
+            if d < smallest_d:
+                closest_node = row["nid"]
+                smallest_d = d
+    return(closest_node)
+
 def find_start_edge(traj, edges):
     closest_edge = None
     if len(traj) != 0:
@@ -93,6 +106,23 @@ def find_local_coords(edge, edge_df, graph):
         startNode = edge_df[edge_df["eid"] == edge]["startnode"].values[0]
         endNode = edge_df[edge_df["eid"] == edge]["endnode"].values[0]
     return(None)
+
+def convert_to_node_list(traj, node_df, node_0):
+    visited_nodes = [node_0]
+    for index, traj_coord in traj.iterrows():
+        traj_lon = traj_coord["Longitude"]
+        traj_lat = traj_coord["Latitude"]
+        local_node_df = node_df[node_df["lon"].between(traj_lon-0.001, traj_lon+0.001)
+                                & node_df["lat"].between(traj_lat-0.001, traj_lat+0.001)]
+        smallest_d = math.inf
+        for index, node_coord in local_node_df.iterrows():
+            d = euclid_d(traj_lon, traj_lat, node_coord["lon"], node_coord["lat"])
+            if d < smallest_d:
+                closest_node = node_coord["nid"]
+                smallest_d = d
+            if closest_node != visited_nodes[-1]:
+                visited_nodes.append(closest_node)
+    return(visited_nodes)
 
 def convert_to_edge_list(traj, edge_df, edge_0):
     visited_edges = [edge_0]
